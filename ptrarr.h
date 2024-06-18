f98ceba1535db7021d11728a741da96d3c2cf625
idko2004 <ivancito72011@gmail.com>
2024-06-18 19:55:55 -0400

    ptrarr

---
+++ ptrarr.h
@@ -0,0 +1,23 @@
+#ifndef PTRARR_H  /* Include guard */
+#define PTRARR_H
+
+typedef struct
+{
+	int spaces_reserved; //La cantidad de punteros que puede guardar str_arr, se cuenta de la misma forma que length
+	int length; //La cantidad de punteros que str_arr tiene actualmente
+	char ** ptr_arr; //La array donde se guardan los punteros hacia los strings
+} Ptrarr;
+
+Ptrarr * ptrarr_new(int capacity); //Inicializa Strarr, debe ejecutarse justo después de crear Strarr, capacity debe indicar la cantidad de espacios a reservar, se cuenta de la misma forma que length
+
+int ptrarr_expand(Ptrarr * strarr, int new_capacity); //Expande el array (str_arr) a la capacidad indicada, la capacidad indicada será el nuevo spaces_reserved
+
+int ptrarr_push(Ptrarr * strarr, void * p); //Añade un string al final del array
+
+void * ptrarr_get(Ptrarr * strarr, int index); //Obtiene el string guardado en el index especificado del array
+
+int ptrarr_set(Ptrarr * strarr, int index, void * p); //Reemplaza un string que ha sido guardado en el array anteriormente en el index indicado
+
+int ptrarr_destroy_everything(Ptrarr * strarr); //Libera la memoria del struct y de todos los strings que contenía
+
+#endif
